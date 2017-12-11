80c45ee3a2cd194c6793ed436febc1d4b7c980f6
elainelinlinlin <elin91793@gmail.com>
2017-12-10 21:01:30 -0800

    improve readability in command line

--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-
+echo ""
+echo "Running Naive Bayes classifier to label tweets"
+echo ""
 python3 ./naive_bayes.py
+echo "--------------------------------------------"
+echo ""
+echo "Running logistic regression for stock price prediction model"
+echo ""
 python3 ./logistic_regression.py
+echo "--------------------------------------------"
+echo ""
+echo "Running SVM for stock price prediction model"
+echo ""
 python3 ./svm.py AAPL
 python3 ./svm.py MSFT
 
